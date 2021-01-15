03f17b494a365d95c2ab25cb4cc159de1fd05c81
Wenox <wenoxar@gmail.com>
2021-01-15 10:58:22 +0100

    initial commit

---
+++ src/main/java/com/wenox/traffic/TrafficMeterApplication.java
@@ -0,0 +1,13 @@
+package com.wenox.traffic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrafficMeterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrafficMeterApplication.class, args);
+	}
+
+}
